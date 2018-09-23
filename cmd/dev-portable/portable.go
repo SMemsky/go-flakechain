5d3f292be3583ef6b5d9cc3117f92805c7c92d34
SMemsky <schooldev3000@gmail.com>
2018-09-23 20:01:41 +0300

    Minor formattin changes in dev-portable

--- cmd/dev-portable/portable.go
+++ cmd/dev-portable/portable.go
@@ -1,61 +1,61 @@
 package main
 
 import (
-	"fmt"
 	"encoding/hex"
+	"fmt"
 
 	"github.com/SMemsky/go-flakechain/storages/portable"
 )
 
 type CommandHandshakeRequest struct {
-	Node	BasicNodeData	`store:"node_data"`
-	Payload	CoreSyncData	`store:"payload_data"`
+	Node    BasicNodeData `store:"node_data"`
+	Payload CoreSyncData  `store:"payload_data"`
 }
 
 type BasicNodeData struct {
-	LocalTime	uint64	`store:"local_time"`
-	MyPort		uint32	`store:"my_port"`
-	NetworkId	string	`store:"network_id"`
-	PeerId		uint64	`store:"peer_id"`
+	LocalTime uint64 `store:"local_time"`
+	MyPort    uint32 `store:"my_port"`
+	NetworkId string `store:"network_id"`
+	PeerId    uint64 `store:"peer_id"`
 }
 
 type CoreSyncData struct {
-	CumulativeDifficulty	uint64	`store:"cumulative_difficulty"`
-	CurrentHeight			uint64	`store:"current_height"`
-	TopId					string	`store:"top_id"`
-	TopVersion				uint8	`store:"top_version"`
+	CumulativeDifficulty uint64 `store:"cumulative_difficulty"`
+	CurrentHeight        uint64 `store:"current_height"`
+	TopId                string `store:"top_id"`
+	TopVersion           uint8  `store:"top_version"`
 }
 
 type CommandHandshakeResponse struct {
 	// NOTE: Field "local_peerlist" is deprecated and not implemented.
-	Deprecated string `store:"local_peerlist"`
-	Peers	[]PeerListEntry	`store:"local_peerlist_new"`
-	Node	BasicNodeData	`store:"node_data"`
-	Payload	CoreSyncData	`store:"payload_data"`
+	Deprecated string          `store:"local_peerlist"`
+	Peers      []PeerListEntry `store:"local_peerlist_new"`
+	Node       BasicNodeData   `store:"node_data"`
+	Payload    CoreSyncData    `store:"payload_data"`
 }
 type PeerListEntry struct {
-	Address		AddressType	`store:"adr"`
-	Id			uint64		`store:"id"`
-	LastSeen	int64		`store:"last_seen"`
+	Address  AddressType `store:"adr"`
+	Id       uint64      `store:"id"`
+	LastSeen int64       `store:"last_seen"`
 }
 
 type AddressType struct {
-	Address	struct {
-		Ip		uint32	`store:"m_ip"`
-		Port	uint16	`store:"m_port"`
-	}					`store:"addr"`
-	Type	uint8		`store:"type"` // TODO: IPv6 support
+	Address struct {
+		Ip   uint32 `store:"m_ip"`
+		Port uint16 `store:"m_port"`
+	} `store:"addr"`
+	Type uint8 `store:"type"` // TODO: IPv6 support
 }
 
 func testBasic() {
-	test := CommandHandshakeRequest {
-		BasicNodeData {
+	test := CommandHandshakeRequest{
+		BasicNodeData{
 			1536691999,
 			12560,
 			"rnowflakenetwork",
 			17196861955154749857,
 		},
-		CoreSyncData {
+		CoreSyncData{
 			722293175540,
 			90128,
 			";\xaf@\xbbR>#\xf47\xc0\xb9\x86\xab\xd2\x08\xdc\xa4\x07\xeb\\F`\x8eQ\x08~\xc1^\x99Z\xf6\xab",
